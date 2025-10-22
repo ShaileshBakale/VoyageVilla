e8ef95c70d79747dbc2b5bb8713b0f9eaa6a73ba
Shailesh <shaileshbakale2004@gmail.com>
2025-10-22 15:45:33 +0530

    'FirstCommit'

---
+++ models/listing.js
@@ -0,0 +1,23 @@
+// models/listing.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema({
+  filename: String,
+  url: String,
+});
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: imageSchema,   // ✅ now an object!
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
